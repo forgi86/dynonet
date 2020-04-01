749a11b569f81d58d54f891d514a1665d53cbc2d
marco <marco.forgione@supsi.ch>
2020-04-01 17:32:04 +0200

    playing with the WH2016 dataset

---
+++ examples/WH2016/WH2016_test.py
@@ -0,0 +1,146 @@
+import torch
+import pandas as pd
+import numpy as np
+import os
+from torchid.module.LTI import LinearMimo
+import matplotlib.pyplot as plt
+import torch.nn as nn
+import control
+import util.metrics
+
+
+class StaticNonLin(nn.Module):
+
+    def __init__(self):
+        super(StaticNonLin, self).__init__()
+
+        self.net = nn.Sequential(
+            nn.Linear(1, 20),  # 2 states, 1 input
+            nn.ReLU(),
+            nn.Linear(20, 1)
+        )
+
+        #for m in self.net.modules():
+        #    if isinstance(m, nn.Linear):
+        #        nn.init.normal_(m.weight, mean=0, std=1e-3)
+        #        nn.init.constant_(m.bias, val=0)
+
+    def forward(self, y_lin):
+        y_nl = self.net(y_lin)
+        return y_nl
+
+
+
+if __name__ == '__main__':
+
+    # Set seed for reproducibility
+    model_filename = 'model_WH'
+
+    # Settings
+    n_b = 3
+    n_a = 3
+    n_fit = 100000
+    n_batch = 1
+
+    # Column names in the dataset
+    COL_F = 'fs'
+    TAG_R = 'r'
+    TAG_U = 'u'
+    TAG_Y = 'y'
+
+    # Load dataset
+    df_X = pd.read_csv(os.path.join("data", "WH_TestDataset.csv"))
+    idx_u = list(df_X.keys()).index('u')
+    n_real = idx_u
+    col_names = [TAG_R + str(i) for i in range(n_real)] \
+                + [TAG_U + str(i) for i in range(n_real)] \
+                + [TAG_Y + str(i) for i in range(n_real)] + [COL_F] + ['?']
+    df_X.columns = col_names
+
+    # Extract data
+    y_meas = np.array(df_X[['y1']], dtype=np.float32)
+    u = np.array(df_X[['u1']], dtype=np.float32)
+    fs = np.array(df_X[COL_F].iloc[0], dtype = np.float32)
+    N = y_meas.size
+    ts = 1/fs
+    t = np.arange(N)*ts
+
+    t_fit_start = 0
+    t_fit_end = 100000
+    t_test_start = 100000
+    t_test_end = 188000
+    t_skip = 1000
+
+    # In[Instantiate models]
+
+
+    # Create models
+
+    # Second-order dynamical system
+    G1 = LinearMimo(1, 1, n_b, n_a)
+    G2 = LinearMimo(1, 1, n_b, n_a)
+    # Static sandwitched non-linearity
+    F_nl = StaticNonLin()
+
+
+    # Create model parameters
+    G1.load_state_dict(torch.load(os.path.join("models", f"{model_filename}_G1.pkl")))
+    F_nl.load_state_dict(torch.load(os.path.join("models", f"{model_filename}_F_nl.pkl")))
+    G2.load_state_dict(torch.load(os.path.join("models", f"{model_filename}_G2.pkl")))
+
+    # In[Predict]
+
+    u_torch = torch.tensor(u[None, :])
+    y1_lin = G1(u_torch)
+    y1_nl = F_nl(y1_lin)
+    y_hat = G2(y1_nl)
+
+    # In[Detach]
+    y_hat = y_hat.detach().numpy()[0, :, :]
+    y1_lin = y1_lin.detach().numpy()[0, :, :]
+    y1_nl = y1_nl.detach().numpy()[0, :, :]
+
+    # In[Plot]
+    plt.figure()
+    plt.plot(t, y_meas, 'k', label="$y$")
+    plt.plot(t, y_hat, 'b', label="$\hat y$")
+    plt.plot(t, y_meas - y_hat, 'r', label="$e$")
+    plt.legend(loc='upper left')
+    plt.grid(True)
+
+    # In[Inspect linear model]
+
+#    G1_sys = control.TransferFunction(G1.b_coeff.detach().numpy(), np.r_[1.0, G1.f_coeff.detach().numpy()], ts)
+#    plt.figure()
+#    mag_G1, phase_G1, omega_G1 = control.bode(G1_sys)
+
+#    G2_sys = control.TransferFunction(G2.b_coeff.detach().numpy(), np.r_[1.0, G2.f_coeff.detach().numpy()], ts)
+#    plt.figure()
+#    mag_G2, phase_G2, omega_G2 = control.bode(G2_sys)
+
+    # In[Inspect static non-linearity]
+
+    y1_lin_min = np.min(y1_lin)
+    y1_lin_max = np.max(y1_lin)
+
+    in_nl = np.arange(y1_lin_min, y1_lin_max, (y1_lin_max- y1_lin_min)/1000).astype(np.float32).reshape(-1, 1)
+
+    with torch.no_grad():
+        out_nl = F_nl(torch.as_tensor(in_nl))
+
+    plt.figure()
+    plt.plot(in_nl, out_nl, 'b')
+    plt.plot(in_nl, out_nl, 'b')
+    plt.xlabel('Static non-linearity input (-)')
+    plt.ylabel('Static non-linearity input (-)')
+    plt.grid(True)
+
+    # In[Metrics]
+
+    e_rms = 1000*util.metrics.error_rmse(y_meas, y_hat)[0]
+    fit_idx = util.metrics.fit_index(y_meas, y_hat)[0]
+    r_sq = util.metrics.r_squared(y_meas, y_hat)[0]
+
+    print(f"RMSE: {e_rms:.2f} mV\nFIT:  {fit_idx:.1f}%\nR_sq: {r_sq:.1f}")
+
+
---
+++ examples/WH2016/WH2016_train_single.py
@@ -0,0 +1,186 @@
+import torch
+import pandas as pd
+import numpy as np
+import os
+from torchid.old.linearsiso_TB import LinearDynamicalSystem
+import matplotlib.pyplot as plt
+import time
+import torch.nn as nn
+
+import util.metrics
+
+
+class StaticNonLin(nn.Module):
+
+    def __init__(self):
+        super(StaticNonLin, self).__init__()
+
+        self.net = nn.Sequential(
+            nn.Linear(1, 20),  # 2 states, 1 input
+            nn.Tanh(),
+            nn.Linear(20, 1)
+        )
+
+        for m in self.net.modules():
+            if isinstance(m, nn.Linear):
+                nn.init.normal_(m.weight, mean=0, std=1e-3)
+                nn.init.constant_(m.bias, val=0)
+
+    def forward(self, y_lin):
+        y_nl = self.net(y_lin)
+        return y_nl
+
+
+if __name__ == '__main__':
+
+    # Set seed for reproducibility
+    np.random.seed(0)
+    torch.manual_seed(0)
+
+    # Settings
+    add_noise = True
+    lr = 2e-4
+    num_iter = 40000
+    test_freq = 100
+    n_fit = 100000
+    decimate = 1
+    n_batch = 1
+    n_b = 3
+    n_f = 3
+
+    # Column names in the dataset
+    COL_F = ['fs']
+    COL_U = ['u']
+    COL_Y = ['y']
+
+    # Load dataset
+    df_X = pd.read_csv(os.path.join("data", "WH_CombinedZeroMultisineSinesweep.csv"))
+
+    # Extract data
+    y = np.array(df_X[COL_Y], dtype=np.float32)
+    u = np.array(df_X[COL_U], dtype=np.float32)
+    fs = np.array(df_X[COL_F].iloc[0], dtype = np.float32)
+    N = y.size
+    ts = 1/fs
+    t = np.arange(N)*ts
+
+    # Fit data
+    y_fit = y[:n_fit:decimate]
+    u_fit = u[:n_fit:decimate]
+    t_fit = t[0:n_fit:decimate]
+
+
+    # Prepare data
+    u_fit_torch = torch.tensor(u_fit, dtype=torch.float, requires_grad=False)
+    y_fit_torch = torch.tensor(y_fit, dtype=torch.float)
+
+
+    # Second-order dynamical system custom defined
+    b1_coeff = np.array([0.1, 0.0, 0.0], dtype=np.float32)
+    f1_coeff = np.array([-0.9, 0.0, 0.0], dtype=np.float32)
+    G1 = LinearDynamicalSystem(b1_coeff, f1_coeff)
+    y_init_1 = torch.zeros((n_batch, n_f), dtype=torch.float)
+    u_init_1 = torch.zeros((n_batch, n_b), dtype=torch.float)
+
+    # Second-order dynamical system custom defined
+    b2_coeff = np.array([0.1, 0.0, 0.0], dtype=np.float32)
+    f2_coeff = np.array([-0.9, 0.0, 0.0], dtype=np.float32)
+    G2 = LinearDynamicalSystem(b2_coeff, f2_coeff)
+    y_init_2 = torch.zeros((n_batch, n_f), dtype=torch.float)
+    u_init_2 = torch.zeros((n_batch, n_b), dtype=torch.float)
+
+    # Static sandwitched non-linearity
+    F_nl = StaticNonLin()
+
+    # Setup optimizer
+    optimizer = torch.optim.Adam([
+        {'params': G1.parameters(), 'lr': lr},
+        {'params': G2.parameters(), 'lr': lr},
+        {'params': F_nl.parameters(), 'lr': lr},
+    ], lr=lr)
+
+    # In[Train]
+    LOSS = []
+    start_time = time.time()
+    for itr in range(0, num_iter):
+
+        optimizer.zero_grad()
+
+        # Simulate
+        y1_lin = G1(u_fit_torch, y_init_1, u_init_1)
+        y1_nl = F_nl(y1_lin)
+        y_hat = G2(y1_nl, y_init_2, u_init_2)
+
+        # Compute fit loss
+        err_fit = y_fit_torch - y_hat
+        loss_fit = torch.mean(err_fit**2)
+        loss = loss_fit
+
+        LOSS.append(loss.item())
+        if itr % test_freq == 0:
+            with torch.no_grad():
+                RMSE = torch.sqrt(loss)
+            print(f'Iter {itr} | Fit Loss {loss_fit:.6f} | RMSE:{RMSE:.4f}')
+
+        # Optimize
+        loss.backward()
+
+        if itr == 100:
+            pass
+        optimizer.step()
+
+    train_time = time.time() - start_time
+    print(f"\nTrain time: {train_time:.2f}") # 182 seconds
+
+    # In[Save model]
+    if not os.path.exists("models"):
+        os.makedirs("models")
+    model_filename = "model_WH"
+
+    torch.save(G1.state_dict(), os.path.join("models", f"{model_filename}_G1.pkl"))
+    torch.save(F_nl.state_dict(), os.path.join("models", f"{model_filename}_F_nl.pkl"))
+    torch.save(G2.state_dict(), os.path.join("models", f"{model_filename}_G2.pkl"))
+
+    # In[To numpy]
+
+    y_hat = y_hat.detach().numpy()
+    y1_lin = y1_lin.detach().numpy()
+    y1_nl = y1_nl.detach().numpy()
+
+    # In[Plot]
+    plt.figure()
+    plt.plot(t_fit, y_fit, 'k', label="$y$")
+    plt.plot(t_fit, y_hat, 'b', label="$\hat y$")
+    plt.legend()
+
+    plt.figure()
+    plt.plot(LOSS)
+    plt.grid(True)
+
+    # In[Plot static non-linearity]
+
+    y1_lin_min = np.min(y1_lin)
+    y1_lin_max = np.max(y1_lin)
+
+    in_nl = np.arange(y1_lin_min, y1_lin_max, (y1_lin_max- y1_lin_min)/1000).astype(np.float32).reshape(-1, 1)
+
+    with torch.no_grad():
+        out_nl = F_nl(torch.as_tensor(in_nl))
+
+    plt.figure()
+    plt.plot(in_nl, out_nl, 'b')
+    plt.plot(in_nl, out_nl, 'b')
+    #plt.plot(y1_lin, y1_nl, 'b*')
+    plt.xlabel('Static non-linearity input (-)')
+    plt.ylabel('Static non-linearity input (-)')
+    plt.grid(True)
+
+    # In[Plot]
+    e_rms = util.metrics.error_rmse(y_hat, y_fit)[0]
+    print(f"RMSE: {e_rms:.2f}")
+
+
+
+
+
+
